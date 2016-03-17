ce5301ef084694f81b86b37274a7ad05928d2ee7
= <caleb.scholze@gmail.com>
2016-03-17 10:02:07 -0500

    modified UI to include cards for discovery results

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,6 +3,17 @@ const discoverMeControllers = angular.module('discoverMeControllers', []);
 discoverMeControllers.controller('DashboardCtrl', ['$scope',
   function ($scope) {
     $scope.localhost = '127.0.0.1';
+
+    $scope.discoveryResults = {
+      upHosts: [
+        '192.168.0.1',
+        '192.168.0.4',
+        '192.168.0.24',
+        '10.0.0.4',
+        '10.0.0.5'
+        ]
+      };
+
   }
 ]);
 
